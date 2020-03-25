a7de89af8412aab9fc211a86c3a37eb04fef678c
KrishanpalSinghRajput1999 <55198551+KrishanpalSinghRajput1999@users.noreply.github.com>
2020-03-25 17:57:47 +0530

    Final_program
    The final program for the program driven processes has been made.
    All the processes with their respective timings are given in the program itself.

--- Code1.c
+++ Code1.c
@@ -2,10 +2,10 @@
 
 /*
 Process 	Arrival time 	Burst time
-P1 			    0 				   10
-P2 			    3				     26
-P3 			   11 				    9
-P4 			   14 				   24
+P1 			 0 	  10
+P2 			 3	  26
+P3 			11         9
+P4 			14 	  24
 */
 
 #include<stdio.h>
@@ -18,35 +18,101 @@ main()
 	int burst_time[4]={10,26,9,24};  //list the estimated run time of each process.
 	int burst[4]={10,26,9,24};   
 	int arrived_process[4]={1};
-	double priority[4]={0};  //list the priority of each process.
+	/*list the priority of each process.
+	  double is choosen because of floating point values of priority*/
+	double priority[4]={0};  
 	int complete[4]={0}; //list the complete processes.
   int tm=0,i=0,bt=0;
 	while(1)
 	{
-		int k;
-		for(k=0;k<4;k++)
+	  int k;
+	  for(k=0;k<4;k++)
+	   {
+	      if(tmr>=arrival_time[k] && burst[k]!=(-1)) 
 		{
-			if(tmr>=arrival[k] && burst[k]!=(-1)) 
+		  priority[k]=1+((tmr-arrival_time[k])/burst[k]);
+		}
+	   }
+	  int p,q;
+	  for(p=0;p<4;p++)
+	   {
+	     for(q=0;q<4-1-p;q++)
+		{
+		  if(priority[q]<priority[q+1] && arrived_process[q]!=0 && arrived_process[q+1]!=0)
+		      {
+			double t1=priority[q];
+			priority[q]=priority[q+1];
+			priority[q+1]=t1;
+			int t2=arrived_process[q];
+			arrived_process[q]=arrived_process[q+1];
+			process_arrived[q+1]=t2;
+		      }
+		  else 
+		      if(priority[q]==priority[q+1]  && arrived_process[q]!=0 && arrived_process[q+1]!=0)
 			{
-				priority[k]=1+((tmr-arrival_time[k])/burst[k]);
+			  if(burst[q]<burst[q+1])
+			     {
+				int t=arrived_process[q];
+				arrived_process[q]=arrived_process[q+1];
+				arrived_process[q+1]=t;
+			     }
 			}
 		}
-		
+	    }
+		int y=arrived_process[0];
+		bt=burst[y-1];
+		do
+		{
+			//printf("timer : %d",tm);
+			int x;
+			for(x=0;x<4;x++)
+			{
+				if(tm==arrival_time[x])
+				{		
+					arrived_process[x]=x+1;
+					printf("process %d arrived\n",x);
+				}	
+			}
+			bt=bt-1;
+			tm=tm+1;
+			if(bt==0)
+			{
+				printf("process %d completed\n",y-1);
+				complete[y-1]=tim;
+				i=i+1;
+				burst[y-1]=-1;
+				//priority[0]=0;
+				for(x=0;x<4;x++)
+				{
+					priority[x]=0;
+					if(arrived_process[x]!=0)
+					{
+						arrived_process[x]=x+1;
+					}
+				}
+				break;
+			}
+		}while(1);
+		if(i==4)
+			break;
+
+
 		
 	}	
-	int turnaround_time[4]={0};
-	int waiting_time[4]={0};
+	
+	int tat[4]={0}; //turnaround_time
+	int wait_time[4]={0};
 	for(i=0;i<4;i++)
 	{
-		turnaround_time[i]=complete[i]-arrival[i];
-		waiting_time[i]=turnaround_time[i]-burst_time[i];
+		tat[i]=complete[i]-arrival_time[i];
+		wait_time[i]=tat[i]-burst_time[i];
 	}
 	printf("process\tarrival_time\tburst_time\tturnaround_time\twaiting_time\n");
 	for(i=0;i<4;i++)
 	{
-		printf("i\t%d\t\t%d\t\t%d\t\t%d\n",i,arrival[i],burst_time[i],turnaround_time[i],waiting_time[i]);
+		printf("%d\t%d\t\t%d\t\t%d\t\t%d\n",i,arrival_time[i],burst_time[i],tat[i],wait_time[i]);
 	}
-	printf("average waiting time is %f .",(waiting_time[0]+waiting_time[1]+waiting_time[2]+waiting_time[3])/4.0);
-	printf("average turnaround time is %f .",(turnaround_time[0]+turnaround_time[1]+turnaround_time[2]+turnaround_time[3])/4.0);
+	printf("average waiting time is %f .",(wait_time[0]+wait_time[1]+wait_time[2]+wait_time[3])/4.0);
+	printf("average turnaround time is %f .",(tat[0]+tat[1]+tat[2]+tat[3])/4.0);
 		
 }
